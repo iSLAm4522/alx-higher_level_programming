092b1a0ebd614596568cdfffc61d28a3188324b9
iSLAm4522 <islammadian798@gmail.com>
2025-04-17 00:19:04 +0200

    Add 9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define a class Rectangle that inherits from BaseGeometry."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class that represents a rectangle."""
+    def __init__(self, width, height):
+        """Initialize a rectangle with width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Override the area method to calculate the rectangle's area."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return a string representation of the rectangle."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
