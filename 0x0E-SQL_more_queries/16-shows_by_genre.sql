1a0a561c7c8861d49fb6528183ced4b105795880
iSLAm4522 <islammadian798@gmail.com>
2025-06-07 09:23:35 +0300

    Add 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,14 +1,11 @@
 -- This query retrieves a list of TV show titles along with their associated genre names.
--- It performs a LEFT JOIN between the 'tv_shows' table and a derived table (R2) that maps show IDs to genre names.
--- The derived table (R2) is created by joining 'tv_genres' and 'tv_show_genres' on genre ID.
--- The LEFT JOIN ensures all TV shows are included, even if they have no associated genre (genre name will be NULL).
+-- It uses LEFT JOINs to ensure all TV shows are included, even if they do not have a genre.
 -- The results are ordered alphabetically by TV show title and then by genre name.
-SELECT tv_shows.title, R2.name
+-- Columns returned:
+--   - title: The title of the TV show (from tv_shows table)
+--   - name: The name of the genre (from tv_genres table; may be NULL if no genre is associated)
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows 
-LEFT JOIN (
-    SELECT tv_show_genres.show_id, tv_genres.name
-    FROM tv_genres JOIN tv_show_genres 
-    ON tv_genres.id = tv_show_genres.genre_id
-) AS R2
-ON tv_shows.id = R2.show_id
-ORDER BY tv_shows.title, R2.name;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
