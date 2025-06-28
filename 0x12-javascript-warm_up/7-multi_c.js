02b9d166c3c7d113a9b474b1919b5cdf466115ab
iSLAm4522 <islammadian798@gmail.com>
2025-06-28 16:58:59 +0300

    Add 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+const str = 'C is fun';
+let times = parseInt(argv[2]);
+
+if (isNaN(times)) { console.log('Missing number of occurrences'); } else {
+  while (times--) { console.log(str); }
+}
